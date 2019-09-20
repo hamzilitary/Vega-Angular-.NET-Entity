c198f31eaf6a35d3aad25af593a8df03dc134a60
Hamza Naeem <hamza.naeem@cygrpdal.local>
2019-09-19 19:47:39 -0500

    add Login auth0 in Navbar

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  constructor(private authService: AuthService) {}
   isExpanded = false;
 
   collapse() {
